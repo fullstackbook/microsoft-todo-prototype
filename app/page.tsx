b0327d01492eaf874afd55a79071862573575eeb
Travis Luong <travis@travisluong.com>
2024-02-28 17:40:20 +0800

    add sign in btn to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import SignInBtn from "@/components/signin-btn";
 
 export default function Home() {
   return (
     <div>
       <ModeToggle />
+      <SignInBtn />
     </div>
   );
 }
